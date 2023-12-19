8513ee8ecd184f5f5baf1ada4dce9ae8c7317d77
mehmet dogan <mehmetseymen91@gmail.com>
2023-12-19 13:22:00 +0300

    3. poreje bitti

--- react-dates-reminder-app/src/App.js
+++ react-dates-reminder-app/src/App.js
@@ -1,22 +1,37 @@
+import "./App.css";
+import data from "./data.js";
+import { useState } from "react";
 
 function App() {
+  const [holidays, setHolidays] = useState(data);
+  console.log(data);
+
+// const deletes = (id) => {
+// const silici = holidays.filter((item)=>item.id !==id)
+// setHolidays(silici)
+// }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>National Holidays Dates</h3>
+        {holidays.map((holiday) => {
+          return (
+            <div className="holiday" key={holiday.id}>
+              <img src={holiday.img} alt="img" />
+              <div>
+                <p>{holiday.title}</p>
+                <p>
+                  {holiday.date} 
+                </p>
+                {/* <button onClick={()=>deletes(holiday.id)}>X</button> */}
+              </div>
+            </div>
+          );
+        })}
+        <button onClick={() => setHolidays([])}>Clear All</button>
+      </section>
+    </main>
   );
 }
 
