23f8188f3a53cac7e85835f42d02872ba8bcf553
mehmet dogan <mehmetseymen91@gmail.com>
2023-12-27 18:40:48 +0300

    new project start

---
+++ react-rondom-profile-genarator-app/src/App.js
@@ -0,0 +1,13 @@
+import Randaoprofile from "./componenets/Randaoprofile";
+import "./index.css";
+
+
+function App() {
+  return (
+    <div className="App">
+<Randaoprofile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-rondom-profile-genarator-app/src/componenets/Randaoprofile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Randaoprofile = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Randaoprofile
