dc23ceabfad223da26d7817b8f06530389f3c613
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-03 00:53:22 +0300

    dicital ocloc ok

--- react-digital-clock-app/src/App.js
+++ react-digital-clock-app/src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(time);
+
+  const times = () => {
+    let time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  };
+  setInterval(() => {
+    times();
+  }, 1000);
+
   return (
     <div className="App">
-  
+      <h1>{currentTime}</h1>
     </div>
   );
 }
