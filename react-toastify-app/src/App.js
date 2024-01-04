6f5cda1c2a939893d74863800aef3185bfcff809
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-04 17:58:07 +0300

    new proje

---
+++ react-toastify-app/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const notify = () =>
+    toast.warn("Yanlış Parola...", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  return (
+    <div className="App">
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </div>
+  );
+}
+
+export default App;
