3a650a7bc33af514b6a24609ed596fe3a1e3fcd6
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-04 18:04:30 +0300

    css ok

--- react-toastify-app/src/App.js
+++ react-toastify-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "colored",
+      theme: "dark",
     });
   return (
     <div className="App">
@@ -27,7 +27,7 @@ function App() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="colored"
+        theme="light"
       />
     </div>
   );
