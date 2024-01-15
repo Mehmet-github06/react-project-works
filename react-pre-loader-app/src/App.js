1b6284a6d121485abf9fafd42369bfa71c4c0f51
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-15 17:39:47 +0300

    loading ok

--- react-pre-loader-app/src/App.js
+++ react-pre-loader-app/src/App.js
@@ -1,11 +1,38 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 import Animation from "./component/Animeation";
+import { useEffect } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
 
 function App() {
+  let [loading, setLoading] = useState(false);
+
+
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  });
+
   return (
-    <div className="App">
-   <Animation/>
+    <div >
+      <Animation />
+      <div className="App">
+      {loading ? (
+        <HashLoader
+          loading={loading}
+          color="#ff0000"
+          size={150}
+          aria-label="Loading Spinner"
+        />
+      ) : (
+        <img src="" alt="anime" />
+      )}
+      </div>
+  
     </div>
   );
 }
