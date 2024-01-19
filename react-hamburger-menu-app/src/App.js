c6a1221cfb3eacf386ed7b56ebd60efa909b3e93
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-19 21:49:19 +0300

    navbar.css ok

--- react-hamburger-menu-app/src/App.js
+++ react-hamburger-menu-app/src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 
 function App() {
 
-  
+  const [burger_class,setBurgerClass]= useState("burger-bar unclicked")
+  const [menu_class,setMenuClass]= useState("menu hidden")
+  const [isMenuClicked,setIsMenuClicked]= useState("false")
+
 
 
   return (
