6cd11f385c105765005c048dadd05e6780a60288
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-03 23:55:20 +0300

    revize

--- react-Easy-website-for-beginners-(react-icons)/src/App.js
+++ react-Easy-website-for-beginners-(react-icons)/src/App.js
@@ -2,22 +2,44 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
 
 export default App;
+<p align="center"><img src="https://i.imgur.com/A6bWGFl.gif"/>
+
+<h1 align="center">Hi 👋, I'm MEHMET DOĞAN</h1>
+
+
+
+
+<p align="left"> <a href="https://twitter.com/" target="blank"><img src="https://img.shields.io/twitter/follow/?logo=twitter&style=for-the-badge" alt="" /></a> </p>
+
+🌱 I’m currently learning **WEB Development Front-End**
+<img align="right" alt="Coding" width="300" src="https://i.pinimg.com/originals/81/17/8b/81178b47a8598f0c81c4799f2cdd4057.gif">
+
+
+<h3 align="left">Connect with me:</h3> 
+<div align="left">
+   <a href=https://www.linkedin.com/in/furkan-dogu/ target="_blank"> <img src="https://img.shields.io/static/v1?message=LinkedIn&logo=linkedin&label=&color=0077B5&logoColor=white&labelColor=&style=for-the-badge" height="35" alt="linkedin logo"  /></a>
+</div> 
+
+- 💻 My personel website: <a href="https://furkandogu.vercel.app/" target="_blank">https://furkandogu.vercel.app</a>
+
+- 📫 My email address: **furkandogu2018@gmail.com**
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=furkan-dogu&label=Profile%20views&color=0e75b6&style=flat" alt="furkan-dogu" /> </p>
+
+
+<br>
+<h3 align="left">🚀 Languages and Tools Skills 🚀</h3>
+<p align="left">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=html,css,js,react,redux,py,bootstrap,tailwind,materialui,sass,firebase,git,github,linux,nodejs,vscode" />
+  </a>
+</p>
+
+<h3>My GitHub Stats</h3>
+<img align="right" alt="Coding" width="300" src="https://cdn.dribbble.com/users/1277312/screenshots/14733298/media/39b1045e593737587dd60e42c8422d1f.gif" ></img>
