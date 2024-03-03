3e9c48398cdad01e96706e8f0180185c68d4cc88
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-03 23:58:39 +0300

    revize2

--- react-Easy-website-for-beginners-(react-icons)/src/App.js
+++ react-Easy-website-for-beginners-(react-icons)/src/App.js
@@ -1,45 +1,46 @@
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-     
+      <p align="center"><img src="https://i.imgur.com/A6bWGFl.gif" alt="" /></p>
+      <h1 align="center">Hi 👋, I'm MEHMET DOĞAN</h1>
+
+      <p align="left">
+        <a href="https://twitter.com/" target="_blank">
+          <img src="https://img.shields.io/twitter/follow/?logo=twitter&style=for-the-badge" alt="" />
+        </a>
+      </p>
+
+      <p>🌱 I’m currently learning <strong>WEB Development Front-End</strong></p>
+      
+      <img align="right" alt="Coding" width="300" src="https://i.pinimg.com/originals/81/17/8b/81178b47a8598f0c81c4799f2cdd4057.gif" />
+
+      <h3 align="left">Connect with me:</h3>
+      <div align="left">
+        <a href="https://github.com/Mehmet-github06" target="_blank">
+          <img src="https://img.shields.io/static/v1?message=LinkedIn&logo=linkedin&label=&color=0077B5&logoColor=white&labelColor=&style=for-the-badge" height="35" alt="linkedin logo" />
+        </a>
+      </div>
+
+      <p>💻 My personal website: <a href="https://furkandogu.vercel.app/" target="_blank">https://furkandogu.vercel.app</a></p>
+      <p>📫 My email address: <strong>furkandogu2018@gmail.com</strong></p>
+
+      <p align="left">
+        <img src="https://komarev.com/ghpvc/?username=furkan-dogu&label=Profile%20views&color=0e75b6&style=flat" alt="furkan-dogu" />
+      </p>
+
+      <h3 align="left">🚀 Languages and Tools Skills 🚀</h3>
+      <p align="left">
+        <a href="https://skillicons.dev">
+          <img src="https://skillicons.dev/icons?i=html,css,js,react,redux,py,bootstrap,tailwind,materialui,sass,firebase,git,github,linux,nodejs,vscode" alt="Skills Icons" />
+        </a>
+      </p>
+
+      <h3>My GitHub Stats</h3>
+      <img align="right" alt="Coding" width="300" src="https://cdn.dribbble.com/users/1277312/screenshots/14733298/media/39b1045e593737587dd60e42c8422d1f.gif" alt="GitHub Stats" />
     </div>
   );
 }
 
 export default App;
-<p align="center"><img src="https://i.imgur.com/A6bWGFl.gif"/>
-
-<h1 align="center">Hi 👋, I'm MEHMET DOĞAN</h1>
-
-
-
-
-<p align="left"> <a href="https://twitter.com/" target="blank"><img src="https://img.shields.io/twitter/follow/?logo=twitter&style=for-the-badge" alt="" /></a> </p>
-
-🌱 I’m currently learning **WEB Development Front-End**
-<img align="right" alt="Coding" width="300" src="https://i.pinimg.com/originals/81/17/8b/81178b47a8598f0c81c4799f2cdd4057.gif">
-
-
-<h3 align="left">Connect with me:</h3> 
-<div align="left">
-   <a href=https://www.linkedin.com/in/furkan-dogu/ target="_blank"> <img src="https://img.shields.io/static/v1?message=LinkedIn&logo=linkedin&label=&color=0077B5&logoColor=white&labelColor=&style=for-the-badge" height="35" alt="linkedin logo"  /></a>
-</div> 
-
-- 💻 My personel website: <a href="https://furkandogu.vercel.app/" target="_blank">https://furkandogu.vercel.app</a>
-
-- 📫 My email address: **furkandogu2018@gmail.com**
-
-<p align="left"> <img src="https://komarev.com/ghpvc/?username=furkan-dogu&label=Profile%20views&color=0e75b6&style=flat" alt="furkan-dogu" /> </p>
-
-
-<br>
-<h3 align="left">🚀 Languages and Tools Skills 🚀</h3>
-<p align="left">
-  <a href="https://skillicons.dev">
-    <img src="https://skillicons.dev/icons?i=html,css,js,react,redux,py,bootstrap,tailwind,materialui,sass,firebase,git,github,linux,nodejs,vscode" />
-  </a>
-</p>
-
-<h3>My GitHub Stats</h3>
-<img align="right" alt="Coding" width="300" src="https://cdn.dribbble.com/users/1277312/screenshots/14733298/media/39b1045e593737587dd60e42c8422d1f.gif" ></img>
