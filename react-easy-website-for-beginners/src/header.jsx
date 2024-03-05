f53c0614fbe3c1bdd42fcfe8c1bc7d02b3395c99
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-05 23:50:03 +0300

    css files

---
+++ react-easy-website-for-beginners/src/header.jsx
@@ -0,0 +1,19 @@
+import './header.css';
+
+import React from 'react';
+
+const header = () => {
+  return (
+    <div>
+        <nav>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#£">Contact</a></li>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default header
