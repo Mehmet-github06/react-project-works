e1e134d0e17904f1147335b18a86cc38021c5689
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-05 23:54:53 +0300

    main

---
+++ react-easy-website-for-beginners/src/main.jsx
@@ -0,0 +1,17 @@
+import './main.css';
+
+function main() {
+  return (
+    <div className='site-info'>
+        <div className='title'>
+               MD-SOFT
+        </div>
+        <div className='sub-title'> 
+            WEB DEVELOPER
+        </div>
+      
+    </div>
+  )
+}
+
+export default main
